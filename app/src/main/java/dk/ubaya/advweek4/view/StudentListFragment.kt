b40a38339ce67f4d0e231ddb1875e2a6d8c05b54
David Kurniawan <s160819001@student.ubaya.ac.id>
2022-03-10 10:17:16 +0700

    week 5 10.17

--- app/src/main/java/dk/ubaya/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/dk/ubaya/advweek4/view/StudentListFragment.kt
@@ -36,6 +36,14 @@ class StudentListFragment : Fragment() {
         recView.layoutManager = LinearLayoutManager(context)
         recView.adapter = studentListAdapter
 
+        refreshLayout.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
         observeViewModel()
 
     }
@@ -49,13 +57,13 @@ class StudentListFragment : Fragment() {
             txtError.visibility = if (it) View.VISIBLE else View.GONE
         })
 
-        viewModel.loadingDoneLD.observe(viewLifecycleOwner, Observer {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
             if(it) {
-                progressLoad.visibility = View.GONE
-                recView.visibility = View.VISIBLE
-            } else {
                 progressLoad.visibility = View.VISIBLE
                 recView.visibility = View.GONE
+            } else {
+                progressLoad.visibility = View.GONE
+                recView.visibility = View.VISIBLE
             }
         })
     }
--- app/src/main/java/dk/ubaya/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/dk/ubaya/advweek4/viewmodel/ListViewModel.kt
@@ -1,22 +1,54 @@
 package dk.ubaya.advweek4.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dk.ubaya.advweek4.model.Student
+import dk.ubaya.advweek4.view.MainActivity
 
-class ListViewModel:ViewModel() {
+class ListViewModel(application: Application):AndroidViewModel(application) {
     val studentsLD = MutableLiveData<List<Student>>()
     val loadingErrorLD = MutableLiveData<Boolean>()
-    val loadingDoneLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    private val TAG = "volleyTag"
+    private var queue:RequestQueue ?= null
 
     fun refresh() {
-        val student1 = Student("34-7637846","Gilberte","2002/04/09","9505044384","http://dummyimage.com/100x75.png/cc0000/ffffff")
-        val student2 = Student("01-9234987","Wynnie","1995/01/27","5495093961","http://dummyimage.com/100x75.png/cc0000/ffffff")
-        val student3 = Student("17-1563651","Cathee","1998/11/02","3257640965","http://dummyimage.com/100x75.png/5fa2dd/ffffff")
+        loadingErrorLD.value=false
+        loadingLD.value=true
 
-        studentsLD.value = arrayListOf<Student>(student1, student2, student3)
-        loadingErrorLD.value = false
-        loadingDoneLD.value = true
+        queue = Volley.newRequestQueue(getApplication())
+        var url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(Request.Method.GET, url, {
+            response->
+            val sType = object : TypeToken<List<Student>>(){}.type
+            val result = Gson().fromJson<List<Student>>(response,sType)
+            studentsLD.value = result
+
+            loadingLD.value=false
+            Log.d("showvolley",response.toString())
+        },{
+            loadingErrorLD.value=true
+            loadingLD.value=false
+            Log.e("showvolley",it.toString())
+        })
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
     }
 
 }
